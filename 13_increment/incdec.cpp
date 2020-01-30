8e4e8f65d60df34b4a271f7c0d83dd4fc60f1b6b
Edo Yulistama <edo.yulistama@erloom.id>
2020-01-30 21:43:20 +0700

    13_increment

---
+++ 13_increment/incdec.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int a = 5;
+	int b = 6;
+
+	// increment dan decrement
+	// preincremen dan postincrement
+
+	// postincrement
+	cout << a << endl;
+	cout << a++ << endl;
+	cout << a << endl << endl;
+
+	// preincrement
+	cout << b << endl;
+	cout << ++b << endl;
+	cout << b << endl << endl;
+	cin.get();
+	return 0;
+}
